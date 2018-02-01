4fc9a3c71c16e8ad2ed5fae0ef53d02bf75a4a28
BuildTools <unconfigured@null.spigotmc.org>
2018-02-01 09:33:12 -0600

    Add project files.

---
+++ zenDiscord/Modules/Commands.cs
@@ -0,0 +1,34 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zenDiscord.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("say")]
+        [Alias("say", "Say")]
+        public async Task SayAsync([Remainder] string msg)
+        {
+            await ReplyAsync(msg.ToString());
+        }
+
+        [Command("help")]
+        [Alias("help", "Help")]
+        public async Task HelpAsync()
+        {
+            await ReplyAsync("__**zenDiscord Bot**__\n!info - Information about the bot\n!say - Make the bot talk!");
+        }
+
+        [Command("info")]
+        [Alias("info", "Info")]
+        public async Task InfoAsync()
+        {
+            await ReplyAsync("__**zenDiscord Bot**__\n! Created by Motley");
+        }
+    }
+    
+}
---
+++ zenDiscord/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+
+//
+//
+//
+//
+//
+//          zenDiscord - The open source Discord bot
+//           
+//          Coded by Motley
+//          https://hackforums.net/member.php?action=profile&uid=2368933
+//
+//
+//
+//
+//
+
+namespace zenDiscord
+{
+    class Program
+    {
+
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;            
+
+
+        public async Task RunBotAsync()
+        {
+            Console.WriteLine("zenDiscord - The open source Discord Bot/nCoded by Motley/nhttps://hackforums.net/member.php?action=profile&uid=2368933");
+
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+
+            // INSERT BOT TOKEN HERE
+
+            string botToken = "INSERT BOT TOKEN HERE";
+
+            //
+
+
+
+
+            _client.Log += Log;
+
+            await RegisterCommandAsync();
+
+            await _client.LoginAsync(TokenType.Bot, botToken);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task RegisterCommandAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+
+            var message = arg as SocketUserMessage;
+            if (message is null || message.Author.IsBot) return;
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("!", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                    var context = new SocketCommandContext(_client, message);
+
+                    var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                    if (!result.IsSuccess)
+                        Console.WriteLine("{" + DateTime.Now.ToString() + "} - " + result.ErrorReason);
+            }
+        }
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            var logSev = Console.ForegroundColor;
+            switch (arg.Severity)
+            {
+                case LogSeverity.Critical:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    return Task.CompletedTask;
+                    break;
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    return Task.CompletedTask;
+                    break;
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    return Task.CompletedTask;
+                    break;
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    return Task.CompletedTask;
+                    break;
+                case LogSeverity.Verbose:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    return Task.CompletedTask;
+                    break;
+                case LogSeverity.Debug:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    return Task.CompletedTask;
+                    break;
+            }
+
+            Console.WriteLine($"{DateTime.Now,-19} [{arg.Severity,8}] {arg.Source}: {arg.Message}");
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+            return Task.CompletedTask;
+        }
+    }
+}
